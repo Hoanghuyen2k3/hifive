da8954043d13960d0edf6b15a36e783d6b6f2fd2
Hoanghuyen2k3 <khanhhuyenx20@gmail.com>
2023-12-28 20:51:04 -0500

    add AI detect hand gestures

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Board from './components/tictactoe/Board';
 import Tictactoe from './components/tictactoe/Tictactoe';
+import RockPapperScissors from './components/rock_scisscors_papper/RockPapperScissors';
 function App() {
   return (
     <div className="App">
-        <Tictactoe />
+        {/* <Tictactoe /> */}
+        <RockPapperScissors />
     </div>
   );
 }
